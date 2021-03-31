3369492846812901e344776a9b05d4726aca3466
Bryce Miller <bmiller@mid-city.com>
2021-03-31 15:23:37 -0400

    fetch adtl event info for success page.

--- pages/events/registration-success.vue
+++ pages/events/registration-success.vue
@@ -2,8 +2,46 @@
   <main class="container">
     <h1>Thank you!</h1>
     <p>
-      We have processed your registration for event ID: {{ $route.params.id }}.
-      You should recevie a confirmation email shortly.
+      We have processed your registration for
+      {{ event.course.title }}. You should recevie a confirmation email shortly.
     </p>
   </main>
 </template>
+<script>
+import { gql } from 'nuxt-graphql-request'
+export default {
+  data() {
+    return {
+      event: {},
+    }
+  },
+  async fetch() {
+    const query = gql`
+      query eventEntryQuery {
+        event(id: "${this.$route.query.event}") {
+          startDatetime
+          endDatetime
+          course {
+            slug
+            title
+            description
+            heroImage {
+              url
+            }
+          }
+          classroom {
+            organization
+            city
+            state
+            timeZone
+          }
+        }
+      }
+    `
+
+    this.event = await this.$graphql.default
+      .request(query)
+      .then((res) => res.event)
+  },
+}
+</script>
