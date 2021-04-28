3d2deed96e45324dcd6046bef656e48b6b536ddd
Bryce Miller <bmiller@mid-city.com>
2021-04-28 10:35:52 -0400

    test auth again

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,11 +78,15 @@ export default {
   },
 
   auth: {
+    redirect: {
+      logout: '/',
+      callback: '/admin',
+    },
     strategies: {
       auth0: {
         domain: process.env.AUTH0_DOMAIN,
         clientId: process.env.AUTH0_CLIENT_ID,
-        logoutRedirectUri: 'https://training.mid-city.com/', // change for prod
+        logoutRedirectUri: 'https://training.mid-city.com/',
       },
     },
   },
