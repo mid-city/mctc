65bf3a6be3127a87a4cb17ee0d69f31055cbc483
Bryce Miller <bmiller@mid-city.com>
2021-12-06 16:24:52 -0500

    add events to course pages

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -2,7 +2,11 @@
   <main>
     <div class="body-container">
       <h1>Class Schedule</h1>
-      <EventList :start="start" :end="end" />
+      <EventList
+        :start="start"
+        :end="end"
+        class="grid gap-4 md:grid-cols-2 xl:grid-cols-3 xl:gap-6"
+      />
     </div>
   </main>
 </template>
